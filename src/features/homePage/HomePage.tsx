6c84f53af2383fd2c0bafccec0ac65764da3efe0
eugeniu zgirdan <zgirdan.evg@gmail.com>
2026-01-19 11:18:14 +0200

    fix build

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Element } from 'react-scroll'
 
 import { blocks } from '@/shared/consts/sidebarMenu'
 
@@ -7,9 +6,9 @@ export const HomePage = () => {
   return (
     <main className=" w-full hr:gap-10 pt-20">
       {blocks.map(({ id, Component }) => (
-        <Element key={id} name={id} id={id} className="content-block">
+        <section key={id} id={id} className="content-block">
           <Component />
-        </Element>
+        </section>
       ))}
     </main>
   )
